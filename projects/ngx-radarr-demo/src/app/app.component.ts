719244a89af82c180c52b88dc1e66ca5c0be74b1
Will Poulson <will.poulson@glowgreenltd.com>
2019-04-13 14:55:53 +0100

    Tested another basic function to get movies.

--- projects/ngx-radarr-demo/src/app/app.component.ts
+++ projects/ngx-radarr-demo/src/app/app.component.ts
@@ -7,16 +7,30 @@ import { RadarrApiService, Movie } from 'ngx-radarr';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  private moviesInLibrary: Array<Movie> = [];
+  // private moviesInLibrary: Array<Movie> = [];
 
   constructor(private radarr: RadarrApiService) {}
 
   ngOnInit() {
-    this.radarr.getMoviesInLibrary().then((movies) => {
-      this.moviesInLibrary = movies;
-      console.log(this.moviesInLibrary);
+    // this.radarr.getMoviesInLibrary(true).then((movies) => {
+    //   const list = []
+    //   movies.sort((a, b) => {
+    //     return b.sizeOnDisk - a.sizeOnDisk;
+    //   });
+
+    //   for (const movie of movies) {
+    //     list.push(movie.title + ' ' + (movie.sizeOnDisk / 1073741824).toFixed(3) + ' GB');
+    //   }
+
+    //   console.log(list);
+    // }).catch((error) => {
+    //   console.log('Uh oh there was an error');
+    // });
+
+    this.radarr.getMovieLookup('The Snowman').then((response) => {
+      console.log(response);
     }).catch((error) => {
-      console.log('Uh oh there was an error');
+      console.log(error);
     });
   }
 }
